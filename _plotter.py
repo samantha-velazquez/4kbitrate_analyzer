c79543fa2c0ccf40910e786b27ab76b7347d7ae5
taruni-manam <taruni.manam@digitalforcetech.com>
2024-08-01 21:02:54 +0000

    changed plotter to original version

--- _plotter.py
+++ _plotter.py
@@ -1,13 +1,13 @@
 import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
 import numpy as np
-from pathlib import Path
+ 
 from _utils import get_mbit_str, get_pretty_codec_name
-
-
-def plot_results(results, graph_title, path):
+ 
+ 
+def plot_results(results, graph_title, graph_filename, user):
     seconds, bitrates, keyframes, encoder = results
-
+ 
     number_of_keyframes = len(keyframes)
     if number_of_keyframes > 75:
         print(f'Warning:\n{str(number_of_keyframes)} I-Frames detected.\n'
@@ -16,13 +16,13 @@ def plot_results(results, graph_title, path):
               'Plotting the bitrate graph without I-frame markings...')
         # drop keyframes
         keyframes = []
-
+ 
     avg_bitrate = get_mbit_str(round(np.mean(bitrates), 2))
     min_bitrate = get_mbit_str(round(min(bitrates), 2))
     max_bitrate = get_mbit_str(round(max(bitrates), 2))
     std_bitrate = get_mbit_str(round(np.std(bitrates), 2))
     encoder = get_pretty_codec_name(encoder)
-
+ 
     # init the plot
     plt.figure(figsize=(19.20, 10.80))
     plt.suptitle(f'{graph_title} | Codec: {encoder}\n\
@@ -31,14 +31,14 @@ def plot_results(results, graph_title, path):
     plt.xlabel('Seconds')
     plt.ylabel('Video Bitrate (Mbps)')
     plt.grid(True)
-
+ 
     # actually plot the data
     bitrate_line, = plt.plot(seconds, bitrates,
                              label=f'Bitrate (Average: {avg_bitrate})')
     # plot vertical lines for keyframes
     for frame in keyframes:
         plt.axvline(frame, color='r', linestyle='--', linewidth=1)
-
+ 
     # create legend for I-frame-lines
     label_text = 'I-Frames' if number_of_keyframes <= 75 else \
                  'Too many I-Frames'
@@ -49,8 +49,7 @@ def plot_results(results, graph_title, path):
                labels=[bitrate_line.get_label(),
                        i_frame_legend.get_label()],
                loc='lower right')
-
+ 
     # save the plot
-    graph_title = Path(graph_title).stem
-    plot_path = path + '/' + graph_title + ".png"
-    plt.savefig(plot_path)
+    path = "/mnt/c/Users/" + user + "/BitRateHistory/" + graph_filename + ".png"
+    plt.savefig(path)
