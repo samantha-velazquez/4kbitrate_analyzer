ab03a8580be2b53e2ef30f27dd4ad644ce2e672b
Taruni Manam <taruni.manam@digitalforcetech.com>
2024-08-06 10:57:52 -0700

    Switched grid to pack and added scrollbar

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ class Window:
     def __init__(self, tk):
         self.tk = tk
         self.geometry = self.tk.geometry('600x400')
+        self.tk.resizable(width = True, height = True)
         self.title = self.tk.title('Bit Rate Analyzer')
+        self.frame = Frame(self.tk)
+        self.frame.pack(pady=10)
+
         self.historyPath = StringVar()
         self.userPath = StringVar()
 
@@ -44,8 +48,9 @@ class Window:
         def CurSelet(evt):
             if self.histbox.size() != 0:
                 value = str(self.histbox.get(ACTIVE))
+                os.chdir(self.historyPath.get())
                 image_path = os.path.abspath(value)
-
+                print(image_path)
                 
                 image = mpimg.imread(image_path)
                 plt.imshow(image)
@@ -53,10 +58,18 @@ class Window:
             else:
                 print("Histbox is empty.")
 
-        self.histlabel = Label(self.tk, text="History", font=('calibre',10, 'bold'))
-        self.histlabel.grid(column=0, row=4)
-        self.histbox = Listbox(self.tk)
-        self.histbox.grid(column=0, row = 6)
+        #self.histlabel = Label(self.tk, text="History", font=('calibre',10, 'bold'))
+        #self.histlabel.grid(column=0, row=4)
+        self.histbox = Listbox(
+            self.frame,
+            #width=25,
+            #height=8,
+
+        )
+        self.histbox.pack(side=LEFT, fill=BOTH)
+
+        self.sb = Scrollbar(self.frame)
+        self.sb.pack(side=RIGHT, fill=BOTH)
         self.histbox.bind('<<ListboxSelect>>', CurSelet)
 
         folder = os.listdir(self.historyPath.get())
@@ -65,42 +78,52 @@ class Window:
                 self.histbox.insert(END, item)
 
     def calculate(self):
+        calc_var = 0
         def getStats(evt): 
             print("clicked")
+            calc_var = 1
             # open file explorer
+            
+            loading_label = Label(self.tk, text = "Loading...")
+            loading_label.pack(side=RIGHT, fill=BOTH)
             filename = filedialog.askopenfilename(initialdir=self.userPath.get(), 
                                                     title="Select a File", 
                                                     filetypes=(("MP4 File", "*.mp4*"), 
                                                                 ("All Files", "*.*")))
             
-            loading_label = Label(self.tk, text = "Loading...")
-            loading_label.grid(column = 3, row = 4)
+            
 
             vid_size = round(os.path.getsize(filename) / 1000000, 1)
             duration = round(float(get_duration(filename)))
-
-            # display file size 
             file_size_label = Label(self.tk, text = "Video Size: " + str(vid_size) + " MB")
-            file_size_label.grid(column=5, row=3)
-
-            # display the video duration
             duration_label = Label(self.tk, text = "Video Duration: " + str(duration) + " seconds")
-            duration_label.grid(column=5, row=4)
-
+            if calc_var > 1:
+                file_size_label.pack_forget()
+                duration_label.pack_forget()
+            # display file size 
+            
+            file_size_label.pack(ipadx=60, ipady=10)
+#side=RIGHT, fill=BOTH
+            # display the video duration
+            
+            duration_label.pack(ipadx=60, ipady=10)
+#side=BOTTOM, pady = 10
             # json holds the function analyze bitrate
             json = analyze_bitrate(filename)
             graph_title = Path(filename).name
             graph_filename = Path(filename).stem
 
             # creates the graph image file
-            plot_results(json, graph_title, graph_filename, self.historyPath.get())    
-            loading_label.grid_forget()
+            plot_results(json, graph_title, graph_filename, self.historyPath.get())
+            calc_var +=1    
+            loading_label.pack_forget()
 
             # add new plots to history.
+        
         self.calcBtn = Button(self.tk, text = "Calculate")
         self.calcBtn.bind('<Button-1>',getStats)
-        self.calcBtn.grid(row=2,column=4)
-
+        self.calcBtn.pack()
+#side=LEFT, padx=20, pady=20
 
 def main():
     root = Tk()
